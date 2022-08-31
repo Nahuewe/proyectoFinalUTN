a4e29d3a3ead37ca69864e73dfd108de69688b86
Nahuewe <nahuelsoriap@gmail.com>
2022-08-31 16:37:03 -0300

    Agregando archivos finales

--- js/app.js
+++ js/app.js
@@ -3,25 +3,25 @@ require(`dotenv`).config()
 const path = require(`path`)
 const mysql = require(`mysql2`)
 
-const app = express()
-const PORT = process.env.PORT || 8080
+// const app = express()
+// const PORT = process.env.PORT || 8080
 
 // Conexion de la DataBase
-const conexion = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-})
+// const conexion = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE
+// })
 
-conexion.connect((err) => {
-    if (err) {
-        console.error(`Error en la conexion ${err.stack}`)
-        return;
-    }
-        console.log(`Conectado a la Base de Datos ${process.env.DATABASE}`);
-    }
-) 
+// conexion.connect((err) => {
+//     if (err) {
+//         console.error(`Error en la conexion ${err.stack}`)
+//         return;
+//     }
+//         console.log(`Conectado a la Base de Datos ${process.env.DATABASE}`);
+//     }
+// ) 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
